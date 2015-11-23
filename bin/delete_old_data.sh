3e29c1fd537abbb7761f239b7585a42b34b84475
Jan <jan.zdrahal@avg.com>
2015-11-23 12:48:59 +0100

    remove also files

--- bin/delete_old_data.sh
+++ bin/delete_old_data.sh
@@ -13,23 +13,30 @@ is_directory() {
     [ -d "$path" ]
 }
 
+show_help() {
+    echo "Usage: <days>[1-*] <directory>"
+}
+
 main() {
     if is_empty $DAYS || is_empty $DIRECTORY; then
-	echo "Mandatory parameter(s) not specified (DAYS, DIRECTORY)."
+	echo "Mandatory parameter(s) not specified."
+	show_help
 	exit 1
     fi
 
     if [ $DAYS -lt 1 ]; then
-	echo "The DAYS parameter must be greater than 0."
+	echo "The <days> parameter must be greater than 0."
+	show_help
 	exit 1
     fi
 
     if ! is_directory $DIRECTORY; then
 	echo "The given path ${DIRECTORY} is not an existing directory."
+	show_help
 	exit 1
     fi
     
     echo "Cleaning directory: ${DIRECTORY}"
-    find $DIRECTORY -depth -mindepth 1 -maxdepth 1 -type d -mtime +$DAYS -exec rm -rfv '{}' \;
+    find $DIRECTORY -depth -mindepth 1 -maxdepth 1 -mtime +$DAYS -exec rm -rfv '{}' \;
 }
 main
